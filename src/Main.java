e92f8ac3cf6dddae0c0c50d3c80eeefd0d776718
Roham <44033386+Rohamvj@users.noreply.github.com>
2018-10-14 16:03:53 +0330

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,128 @@
+import com.sun.org.apache.bcel.internal.generic.Select;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.sql.*;
+
+public class Main extends Application {
+
+
+    public static void main(String[] args) {
+
+
+// Read From Database
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/phonebook", "root", "");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from tbl_person where name='Roham'");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3));
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+/*
+
+//Send To Database
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/phonebook", "root", "");
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("delete from tbl_person");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        */
+
+/*
+        //Send To Database
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/phonebook", "root", "");
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("insert into tbl_person (name, family, phonenumber) values('Yashar','Abbasi',09124445231)");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+*/
+
+        launch(args);
+
+    }
+
+
+    public void start(Stage primaryStage) throws Exception {
+        Button btnViewObj = new Button("View");
+
+        ListView PersonList = new ListView();
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/phonebook", "root", "");
+        Statement PersonStatement = con.createStatement();
+        ResultSet PersonResult = PersonStatement.executeQuery("select * from tbl_person");
+        while (PersonResult.next()) {
+
+            PersonList.getItems().add(PersonResult.getString(1) + "-" + PersonResult.getString(2) + " " + PersonResult.getString(3) + " /" + PersonResult.getString(4));
+        }
+
+/*
+        Button btnSend = new Button("Send");
+
+        TextField NameObj = new TextField();
+        TextField FamilyObj = new TextField();
+        TextField PhoneNumObj = new TextField();
+
+        Label lblName = new Label("Name");
+        Label lblFamily = new Label("Family");
+
+        ListView listView = new ListView();
+        btnSend.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String name = NameObj.getText();
+                String family = FamilyObj.getText();
+
+                listView.getItems().add(name + " " + family);
+            }
+        });*/
+/*
+        GridPane gridPane = new GridPane();
+
+        gridPane.addRow(0, btnSend, lblName, NameObj, lblFamily, FamilyObj);
+
+        gridPane.addColumn(5, listView);
+*/
+        GridPane gridPane = new GridPane();
+        gridPane.addRow(1, PersonList);
+        gridPane.addRow(1, btnViewObj);
+        Scene scene = new Scene(gridPane);
+        primaryStage.setTitle("Phone Book");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+        /*Scene scene = new Scene(gridPane, 600, 600);
+        primaryStage.setTitle("Phone Book");
+        primaryStage.setScene(scene);
+        primaryStage.show();*/
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    private String name;
+    private String family;
+    private int PhoneNum;
+
+    public Person(String name, String family, int PhoneNum) {
+        this.name = name;
+        this.family = family;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public int getAge() {
+        return PhoneNum;
+    }
+}
+
+
